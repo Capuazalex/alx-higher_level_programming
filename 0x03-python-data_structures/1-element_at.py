76efb7c2ede7ec24a081ff9ff2fcd4cd0faa6c18
Capuazalex <rotimiakinlade20@gmail.com>
2023-11-05 06:16:02 -0800

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,11 @@
-#!/usr/bin/python3
-
+#!/usr/bin/pythnon3
 def element_at(my_list, idx):
-    if (idx < 0) or (idx > len(my_list) - 1):
+    if idx < 0 or idx >= len(my_list):
         return None
-    return(my_list[idx])
+    return my_list[idx]
+
+
+my_list = [2, 5, 12, 4, 5]
+idx = 2
+
+print("()".format(element_at(my_list, idx)))
